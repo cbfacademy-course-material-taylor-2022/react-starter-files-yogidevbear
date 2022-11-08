4dba1b3f34e2fd16c3a0b016783b810f3ee19bcf
Andrew Jackson <andrewsyoga@gmail.com>
2022-11-08 19:18:22 +0000

    initial portion of task 1 for react part 3 module

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const name = '[YOUR NAME GOES HERE]';
+const myName = 'Andrew'
+const name = 'aNdReW';
 
 function Profile(name){
+  if (name.toUpperCase() === myName.toUpperCase()) {
     return <main id="content" role="main" className="base">
-            <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
-            </main>
+             <h1>{name}'s React Page</h1>
+             <p>[Your short bio goes here]</p>
+           </main>
+  } else {
+    return <main id="content" role="main" className="base">
+             <h1>Just a React Page</h1>
+             <p>Nothing to see here!</p>
+           </main>
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
+root.render(Profile(name));
 
